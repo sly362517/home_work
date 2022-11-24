08201f381e73de3af74fbd4f483d80b02d39011d
dmitriy <machortovdima@mail.ru>
2022-11-24 18:39:52 +0300

    added task002

--- home_work1/task001/Program.cs
+++ home_work1/task001/Program.cs
@@ -2,8 +2,10 @@
 Console.Write("Введите 1-ое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if(n > m) 
+int m = Convert.ToInt32(Console.ReadLine());  
+if(n == m) 
+    Console.Write("Числа равны");
+else if(n > m) 
     Console.Write(n);
 else 
     Console.Write(m);
---
+++ home_work1/task002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(n == m && n == b) 
+    Console.Write("Числа равны");
+else if(n > m && n > b) 
+    Console.Write(n);
+else if(m > n && m > b) 
+    Console.Write(m);
+else 
+    Console.Write(b);
