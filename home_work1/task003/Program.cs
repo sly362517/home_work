ae5a17107bb3a95d502a2930441423ada1773b54
dmitriy <machortovdima@mail.ru>
2022-11-24 18:48:00 +0300

    added task003

---
+++ home_work1/task003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n % 2 == 0)
+    Console.Write("Чётное число");
+else 
+    Console.Write("Нечётное число");
