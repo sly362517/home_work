d88750c35c3f365500f92b84758dc6d4c88ad8ea
dmitriy <machortovdima@mail.ru>
2022-11-24 18:21:41 +0300

    added task001

---
+++ home_work1/task001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n > m) 
+    Console.Write(n);
+else 
+    Console.Write(m);
