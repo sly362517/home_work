f00eb1601b776728312c70ad22db1aa1747c0f32
dmitriy <machortovdima@mail.ru>
2022-11-28 19:34:49 +0300

    added tsak002 homework2

---
+++ home_work2/task002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 99)
+    {
+        while(n > 1000)
+        n = n / 10;
+    Console.Write(n % 10);
+    }
+else
+    Console.Write("Третьей цифры нет");
