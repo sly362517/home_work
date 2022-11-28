eecd4b7d653944ee0c1d72913e233994b2cd155e
dmitriy <machortovdima@mail.ru>
2022-11-28 19:15:10 +0300

    added tsak003 homework2

---
+++ home_work2/task003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n == 6 || n == 7)
+    Console.Write("Выходной день");
+else
+    Console.Write("Будний день");
