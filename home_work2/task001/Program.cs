782a9f13dd6eaaadd112ae7fa64f2bbf430ca56e
dmitriy <machortovdima@mail.ru>
2022-11-28 18:14:06 +0300

    added tsak001 homework2

---
+++ home_work2/task001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n  < 100 || n > 1000)
+    {
+   Console.Write("Вы ошиблись!\nВведите трёхзначное число: ");
+   n = Convert.ToInt32(Console.ReadLine());
+    } 
+int n1 = n / 10;
+Console.Write(n1 % 10);
