a142c994b38de51e8161a9b80b6cb4eba922908c
dmitriy <machortovdima@mail.ru>
2022-11-30 18:06:41 +0300

    added tsak001 homework3

--- home_work3/task001/Program.cs
+++ home_work3/task001/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+if(length == 5)
+{
+    if (number [0] == number [4] && number[1] == number[3])
+        Console.WriteLine("Палиндром");
+    else
+        Console.WriteLine("Не палиндром");
+}
+else
+    Console.WriteLine("Вы ошиблись!");
