d11fefce46940c4da3a9f9369ad847bd4995bcb8
dmitriy <machortovdima@mail.ru>
2022-12-06 17:41:50 +0300

    added task002 homework4

---
+++ home_work4/task002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+while (i > 0)
+{
+    int number = i % 10;
+    i = i / 10;
+    summa = summa + number;
+}
+Console.WriteLine("Сумма всех цифр в числе равна: " + summa);
