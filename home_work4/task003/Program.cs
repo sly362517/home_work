0720cc4629a6e29d996887610a22f4a3f1e5cd44
dmitriy <machortovdima@mail.ru>
2022-12-06 18:08:16 +0300

    added task003 homework4

---
+++ home_work4/task003/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат: [{string.Join(", ", array)}]"); 
