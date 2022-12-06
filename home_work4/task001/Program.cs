2076170862d60553acce80ffa319d57d26d47c10
dmitriy <machortovdima@mail.ru>
2022-12-06 17:36:46 +0300

    added tsak003 homework4

---
+++ home_work4/task001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int stepen = a;
+
+for (int i = 1; i < b; i++)
+{
+stepen = stepen * a;
+}
+Console.WriteLine("Число A в степени B равно: " + stepen);
