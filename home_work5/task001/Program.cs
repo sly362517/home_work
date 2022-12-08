6d4402f48b3b7e5bd225bb22215f8d48b56125d4
dmitriy <machortovdima@mail.ru>
2022-12-08 16:57:59 +0300

    added task001 homework5

---
+++ home_work5/task001/Program.cs
@@ -0,0 +1,26 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int result = 0;
+    for( int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        result ++;
+    }
+    Console.WriteLine($"Количество чётных чисел: {result}");
+}
+
+
+Console.Clear();
+Console.Write("Введите количество элемнтов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
