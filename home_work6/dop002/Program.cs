f8ca29289bc99de71834925d71551eeb6f7abc99
dmitriy <machortovdima@mail.ru>
2022-12-13 16:15:31 +0300

    added dop 002 homework6

--- home_work6/dop002/Program.cs
+++ home_work6/dop002/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.Clear();
+string[] numbers = Console.ReadLine().Split(" ");
+double x1 = int.Parse(numbers[0]);
+double y1 = int.Parse(numbers[1]);
+double x2 = int.Parse(numbers[2]);
+double y2 = int.Parse(numbers[3]);
+double x3 = int.Parse(numbers[4]);
+double y3 = int.Parse(numbers[5]);
+double A = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+double B = Math.Sqrt(Math.Pow(x1 - x3, 2) + Math.Pow(y1 - y3, 2));
+double C = Math.Sqrt(Math.Pow(x2 - x3, 2) + Math.Pow(y2 - y3, 2));
+double p = (A + B + C) / 2;
+double result = 0;
+result = Math.Round(Math.Sqrt(p * (p - A) * (p - B) * (p - C)), 2);
+Console.WriteLine($"Площадь треугольника равна:  {result}");
