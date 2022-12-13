6e70eb72a17f71da9e30da57edb9a50142453746
dmitriy <machortovdima@mail.ru>
2022-12-13 15:51:44 +0300

    added task002 homework6

---
+++ home_work6/task001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+string[] n = Console.ReadLine().Split(" ");
+int result = 0;
+foreach (string i in n)
+{
+    if (int.Parse(i) > 0)
+    result++;
+}
+Console.WriteLine($"Количество чисел больше 0: {result}");
---
+++ home_work6/task002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b1 - b2)/(k2 - k1);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
