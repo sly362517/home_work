6817af7890b6637eb9f54802c228070ebd4d9bdf
dmitriy <machortovdima@mail.ru>
2022-12-15 16:41:45 +0300

    added task003  homework7

---
+++ home_work7/task001/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 101)) / 10;
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+string[] numbers = Console.ReadLine().Split(" ");
+double[,] matrix = new double[int.Parse(numbers[0]), int.Parse(numbers[1])];
+InputMatrix(matrix);
---
+++ home_work7/task003/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputMatrix(int[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(int[, ] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double summa = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summa += matrix[i, j];
+        }
+        Console.WriteLine($"Столбец {j + 1} = {summa / matrix.GetLength(0)}");
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[, ] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
